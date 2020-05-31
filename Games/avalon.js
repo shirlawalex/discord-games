7774032e165e7b21b83837740c88794946d16fa3
Alex Shirlaw <shirlawalex@gmail.com>
2020-05-31 16:36:50 +0200

    little start of the project

--- Games/avalon.js
+++ Games/avalon.js
@@ -2,8 +2,14 @@ const Games = require('./games')
 
 module.exports = class Avalon extends Games {
 
+  static nameChannel () {
+    let number = Math.floor((Math.random() * 65535) + 4096) //a random number with 4 hexa digit
+    return 'Avalon-' + number.toString(16);
+  }
+
   static helloWorld (message) {
-    message.reply('Channel Avalon created')
+    let msg = 'Channel '+this.nameChannel()+' reated'
+    message.reply(msg)
   }
 
   static matchEmoji (message) {
