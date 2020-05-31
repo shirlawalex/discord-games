0e5a80521b9d4da63693918a7ec8b114d15392fe
Alex Shirlaw <shirlawalex@gmail.com>
2020-05-31 16:40:16 +0200

    Index.js new features in progress -> display presentation -> bot add to guild -> user react to an Emoji -> user send command

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 const Secret = require('./secret.js');
 const Discord = require('discord.js');
-const Avalon = require('./Games/avalon.js');
-// const Games = require('listGames.js');
+const Games = require('./listGames.js');
 const bot = new Discord.Client();
 
+//import JSON file: the text content
+var fs = require("fs");
+var contents = fs.readFileSync("./text.json");
+var text = JSON.parse(contents);
+var lang = "Fr"
 
+var displayText = function (context,key,lang){
+  // console.log(text[context][key][lang])
+  return text[context][key][lang]
+}
+
+//When Bot ready
 bot.on('ready', () => {
   console.log('I am ready!');
 });
 
+//When Bot add to the guild
+/*
+  Display the presentation
+*/
+
+
+//When User react to a Emoji
+/*
+  Start a new game
+*/
+
+
+
+//When User send a message
+/*
+  Execute the command called
+*/
 bot.on('message', (message) => {
-  // Games.matchEmoji(message);
-  Avalon.parse(message)
-  // Avalon.helloWorld(message)
+  Games.parseAll(message);
 });
 
-
-
-  bot.login(Secret.token())
+//Connect bot
+bot.login(Secret.token())
