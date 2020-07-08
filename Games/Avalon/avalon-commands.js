4f8665ea91dc945c5993ba5beae808f6050e74d3
Alex Shirlaw <shirlawalex@gmail.com>
2020-07-08 02:43:30 +0200

    work on Avalon game add the case for the run of the Game add some commands

--- Games/Avalon/avalon-commands.js
+++ Games/Avalon/avalon-commands.js
@@ -4,11 +4,44 @@ const { Discord, fs, displayText, arrayOfFile } = require(`./../../function.js`)
 module.exports  =  {
   commands : [
     {
-      name : 'pingavalon',
+    //   name : 'lang',
+    //   description : 'lang arguement : argument is the language to change',
+    //   execute(bot,games,message, args) {
+    //     if(args.length == 0){
+    //       message.channel.send(games.lang);
+    //     }
+    //     else {
+    //       switch(args[0]){
+    //         case `en` : case `england` : case `angleterre` : case `english` : case `anglais` :
+    //         bot._lang = `En`;
+    //         break;
+    //         case `fr` : case `france` : case `french` : case `francais` : case `français` :
+    //         case `be` : case `belgique` : case `belgium` : case `belge` :
+    //         default :
+    //         games.lang = `Fr`
+    //       }
+    //       message.channel.send(`language set to ${games.lang}`);
+    //     }
+    //     console.log(`language set to ${games.lang}`)
+    //   }
+    // }
+    // ,{
+      name : 'ping',
       description : 'Pong !',
-      execute(bot,message, args) {
+      execute(bot,games,message, args) {
         message.channel.send("Pong!");
       }
     }
-  ]  
+    ,{
+      name : 'rules',
+      description : 'Display rules !',
+      execute(bot,games,message, args) {
+        message.channel.send(games.displayText("menu","welcome"));
+      }
+    }
+  ]
 }
+
+/*
+!
+*/
--- Games/Avalon/avalon-main.js
+++ Games/Avalon/avalon-main.js
@@ -48,6 +48,50 @@ module.exports  = class Avalon extends Games {
       // channel.send(this.displayText("menu","summary"))
       // channel.send(this.displayText("menu","goals"))
       // channel.send(this.displayText("menu","command"))
+      let step = 0;
+      switch (step) {
+        case 0:
+          break;
+        case 1: // Logging players
+          break;
+        case 2: // Starting party
+          break;
+        case 3: // Sorting randomly players
+          break;
+        case 4: // give Role to players
+          break;
+        case 5: // New Leader
+          break;
+        case 6: // Leader start tour
+          break;
+        case 7: // Players vote
+          break;
+        case 8: // Majority of No : Quest doesnt go !
+          break;
+        case 9: // Majority of Yes : Quest go !
+          break;
+        case 10: // Quest Failed : 1 point for Evil
+          break;
+        case 11: // Quest Suceed : 1 point for Good
+          break;
+        case 12: // Number of Quest check
+          break;
+        case 13: // 3 in a row : Ends of Quest
+          break;
+        case 14: // Assassination of Merlin
+          break;
+        case 15: // Evil win
+          break;
+        case 16: // Good win
+          break;
+        case 17: // Credit
+          break;
+        case 18: //
+          break;
+        default:
+          //this case is not implemented
+
+      }
     }).catch(err => {
       console.error("cannot do action from avalon.js");
     })
