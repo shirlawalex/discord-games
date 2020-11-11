7b8f2e0874146d7babd4d9e05c6b8a88a63a40be
window asus <shirlawalex@gmail.com>
2020-11-11 02:43:28 +0100

    re organization of files

--- Games/Avalon/avalon-function.js
+++
@@ -1,7 +0,0 @@
-const Role = require(`../Class/role.js`)
-const Player = require(`../Class/player.js`)
-
-
-// class Joueurs extends Role(Player) {
-
-// }  
---
+++ Games/Avalon/avalon-util.js/avalonRole.js
@@ -0,0 +1,7 @@
+const Role = require(`../../Class/role.js`)
+const Player = require(`../../Class/player.js`)
+
+
+class avalonRole extends Role(Player) {
+
+}  
